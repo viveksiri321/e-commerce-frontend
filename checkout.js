a84638140b151e1b9d90d88924cd347c9070415f
viveksiri321 <vivekgt6061@gmail.com>
2025-06-26 08:45:16 +0530

    Delete checkout.js

--- checkout.js
+++
@@ -1,6 +0,0 @@
-    document.getElementById('checkout-form').addEventListener('submit', function(e) {
-      e.preventDefault();
-      alert("🎉 Order placed successfully!");
-      localStorage.removeItem('cart'); // clear cart
-      window.location.href = 'index.html';
-    });
